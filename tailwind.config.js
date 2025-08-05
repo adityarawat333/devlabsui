6cc252a938a85a84c439fae0c3261d78f63bc92e
Arawat-dlt <aditya.rawat@devlabstechnology.com>
2025-08-05 15:42:57 +0530

    Second Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bevietnam: ['"Be Vietnam Pro"', 'sans-serif'],
+        roboto: ['"Roboto"', 'sans-serif'],
+        sansation: ['"Sansation"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
